d659f122463ce52d847516084067184bf03341e4
Leslie Lau <1178273431@qq.com>
2020-04-13 20:53:05 +0800

    refactor: 自定义 json 响应格式

---
+++ app/Providers/ResponseServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\ServiceProvider;
+
+class ResponseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // 自定义 json 响应格式
+        Response::macro('formatJson', function ($success, $data, $message) {
+            return new JsonResponse([
+                'success' => $success,
+                'data'    => $data,
+                'message' => $message,
+            ]);
+        });
+    }
+}
